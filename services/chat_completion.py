adfc5b0c8a140cd3847b127524add314329fc5c0
bsord <brandon.sorgdrager@gmail.com>
2023-05-11 20:32:37 -0500

    change prompt template to basic agent

--- services/chat_completion.py
+++ services/chat_completion.py
@@ -2,17 +2,21 @@ from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+
 def complete_prompt(prompt: str):
 
     # define the LLM of choice
     llm = OpenAI(temperature=0.0)
 
-    prompt_template = PromptTemplate(
-        input_variables=["product"],
-        template="What is a good name for a company that makes {product}?",
-    )
+    # load tools 
+    tools = load_tools(["llm-math"], llm=llm)
 
-    chain = LLMChain(llm=llm, prompt=prompt_template)
-    completion_response = chain.run("colorful socks")
-    
+    # initialize the agent with our LLM of choice and array of previously defined tools.
+    agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+
+    completion_response = agent.run(" What is the result of 42 raised to the .023 power?")
+
     return completion_response
