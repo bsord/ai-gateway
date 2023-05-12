3a4513b7252bc309081b5d44e40a1f0b52c30bf9
bsord <brandon.sorgdrager@gmail.com>
2023-05-11 22:31:50 -0500

    look ma! my first tool!

--- services/chat_completion.py
+++ services/chat_completion.py
@@ -1,10 +1,11 @@
-from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
-from langchain.chains import LLMChain
 
-from langchain.agents import load_tools
+from langchain.agents import Tool
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
+from langchain.tools import BaseTool, StructuredTool, Tool, tool
+from pydantic import BaseModel, Field
+from tools.custom_tools import GetNewsTool
 
 def complete_prompt(prompt: str):
 
@@ -12,11 +13,18 @@ def complete_prompt(prompt: str):
     llm = OpenAI(temperature=0.0)
 
     # load tools 
-    tools = load_tools(["llm-math"], llm=llm)
+    get_news_tool = GetNewsTool()
+    tools = [
+        Tool(
+            func=get_news_tool,
+            name = "get_news",
+            description="useful for when you need to answer questions about a subreddit"
+        ),
+    ]
 
     # initialize the agent with our LLM of choice and array of previously defined tools.
     agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
-    completion_response = agent.run(" What is the result of 42 raised to the .023 power?")
+    completion_response = agent.run(prompt)
 
     return completion_response
---
+++ tools/custom_tools.py
@@ -0,0 +1,22 @@
+import requests
+from typing import Optional, Type
+from pydantic import Field, BaseModel
+from langchain.tools import BaseTool
+
+class GetNewsSchema(BaseModel):
+    subreddit: str = Field(description="should be a search query")
+
+class GetNewsTool(BaseTool):
+    name = "get_news"
+    description = "useful for when you need to get news about a subreddit"
+    args_schema: Type[GetNewsSchema] = GetNewsSchema
+
+    def _run(self, subreddit: str) -> str:
+        """Use the tool."""
+        print('testing')
+        result = requests.get("https://api.f5.news/posts/{subreddit}".format(subreddit = subreddit))
+        return f"Results: {result.status_code} - {result.text}"
+    
+    async def _arun(self, subreddit: str) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("get_news does not support async")
